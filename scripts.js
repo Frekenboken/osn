e8e15759c1e1dfe744096997a474abc27fbb022c
Pavel Ovchinnikov <pavel.ov07@mail.ru>
2024-12-06 23:51:15 +0300

    update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const Handlebars = require('handlebars');
+
 const burger = document.getElementById('burger');
 const navLinks = document.querySelector('.nav-links');
 const anketa1 = document.getElementById('anketa1');
@@ -7,10 +10,11 @@ burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// anketa1.addEventListener('click', () => {
-//     navLinks.classList.toggle('active');
-// });
+// Чтение файла настроек
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
-// anketa2.addEventListener('click', () => {
-//     navLinks.classList.toggle('active');
-// });
+// Использование настроек
+console.log(`Title: ${config.title}`);
+console.log(`Description: ${config.description}`);
+console.log(`Author: ${config.author}`);
+console.log(`Base URL: ${config.baseUrl}`);
