03cb294b2b31c762770809f025891df0e6c059f7
Pavel Ovchinnikov <pavel.ov07@mail.ru>
2024-12-07 00:13:21 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 const fs = require('fs');
-const Handlebars = require('handlebars');
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
 
 const burger = document.getElementById('burger');
 const navLinks = document.querySelector('.nav-links');
@@ -18,3 +28,7 @@ console.log(`Title: ${config.title}`);
 console.log(`Description: ${config.description}`);
 console.log(`Author: ${config.author}`);
 console.log(`Base URL: ${config.baseUrl}`);
+
+if (getCookie('username') == null) {
+    window.location.href = 'hello.html';
+}
